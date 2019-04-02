b822fcea57b8f27df7ff91fc2ff67a0ae79244d0
craigpullar <craigmichaelpullar@gmail.com>
2019-04-01 21:28:09 -0400

    Wrote curry function and example curried functions

--- src/currying/index.js
+++ src/currying/index.js
@@ -15,3 +15,53 @@ const curriedAdd = curry(add);
 
 console.log(curriedAdd(1));
 console.log(curriedAdd(1)(2));
+
+const get = curry((property, object) => object[property]);
+const objects = [
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 3,
+  },
+];
+
+const ids = objects.map((get('id')));
+console.log(ids);
+
+const getIds = objectsParam => objectsParam.map((get('id')));
+
+console.log(getIds(objects));
+
+const map = curry((fn, values) => values.map(fn));
+
+const getIds2 = map(get('id'));
+
+console.log(getIds2(objects));
+
+const fetchFromServer = () => new Promise((resolve) => {
+  resolve({
+    user: 'Jack',
+    posts: [
+      { title: 'Why curry' },
+      { title: 'Functional programming' },
+
+    ],
+  });
+});
+
+const myRetrievedPosts = fetchFromServer()
+  .then(data => data.posts)
+  .then(posts => posts.map(post => post.title))
+  .then(titles => console.log(titles));
+
+console.log(myRetrievedPosts);
+const myRetrievedPosts2 = fetchFromServer()
+  .then(get('posts'))
+  .then(map(get('title')))
+  .then(console.log);
+
+console.log(myRetrievedPosts2);
